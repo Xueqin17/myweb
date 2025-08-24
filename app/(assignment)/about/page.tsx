973171cc8f0e6ce042b2d8aced41ff07fafe816a
Xueqin HE <22144656@students.latrobe.edu.au>
2025-08-24 21:20:57 +1000

    Add video function to about page

--- app/(assignment)/about/page.tsx
+++ app/(assignment)/about/page.tsx
@@ -1,10 +1,27 @@
 export default function AboutPage() {
   return (
-    <section>
-     
+    
+      <main style={{ padding: 24, maxWidth: 900, margin: '0 auto' }}>
       <p>Name: Xueqin HE</p>
       <p>Student No: 22144656</p>
-      {/* put /public/guide.mp4  */}
-    </section>
+      <p>My CSE5006 Assignment 1 demo video.</p>
+
+      <div style={{ position: 'relative', paddingTop: '50%' }}>
+        <iframe
+          src="https://www.youtube.com/embed/REPLACE_WITH_YOUR_VIDEO_ID" // 
+          title="Assignment 1 Demo"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{
+            position: 'absolute',
+            inset: 0,
+            width: '100%',
+            height: '100%',
+            border: 0
+          }}
+        />
+      </div>
+    </main>
+   
   );
 }
